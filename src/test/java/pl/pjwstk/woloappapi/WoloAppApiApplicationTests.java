6ebb0470b20efd901311c375ebe98a85c10c00d6
Jakwisnie <47974800+Jakwisnie@users.noreply.github.com>
2023-05-05 16:11:16 +0200

    change name of root

---
+++ src/test/java/pl/pjwstk/woloappapi/WoloAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjwstk.woloappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoloAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
