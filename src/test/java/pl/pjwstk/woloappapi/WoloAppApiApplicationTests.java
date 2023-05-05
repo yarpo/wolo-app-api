794aeed38d758b2b95d19d0b18721fb08eba2e1b
Aleksander Mielczarek <40267367+15465@users.noreply.github.com>
2023-05-05 18:07:12 +0200

    Add a simple controller and simple unit tests

--- src/test/java/pl/pjwstk/woloappapi/WoloAppApiApplicationTests.java
+++ src/test/java/pl/pjwstk/woloappapi/WoloAppApiApplicationTests.java
@@ -2,12 +2,23 @@ package pl.pjwstk.woloappapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class WoloAppApiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void testTrue() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testHealthEndpoint() {
+		given()
+				.when().get("/health")
+				.then()
+				.statusCode(200);
 	}
 
 }
