3e18f51edd838ce1c2b3c5a107eca5832dd463b7
Jakwisnie <47974800+Jakwisnie@users.noreply.github.com>
2023-05-05 16:06:18 +0200

    Implement sceleton from start.spring.io
    contains:
    Gradle-groovy
    spring boot 3.0.6
    Spring Web
    Lombok
    PostgreSQL Driver
    JDBC API

---
+++ src/test/java/pl/pjwstk/WoloApp/WoloAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjwstk.WoloApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoloAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
