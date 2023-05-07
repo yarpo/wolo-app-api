6cc0239040968d228a44d961afd27908f1c0d77d
Jakwisnie <47974800+Jakwisnie@users.noreply.github.com>
2023-05-07 18:09:50 +0200

    another version of database

--- src/main/java/pl/pjwstk/woloappapi/controller/Controller.java
+++ src/main/java/pl/pjwstk/woloappapi/controller/Controller.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
+
+
     @GetMapping("/health")
     public ResponseEntity<String> checkHealth() {
         return new ResponseEntity<>(HttpStatus.OK);
--- src/main/java/pl/pjwstk/woloappapi/repository/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package pl.pjwstk.woloappapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import pl.pjwstk.woloappapi.database.Account;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-}
