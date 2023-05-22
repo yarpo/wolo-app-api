62af003baa7556bc127256c96ff4e9c56514b4ab
Jakwisnie <47974800+Jakwisnie@users.noreply.github.com>
2023-05-22 10:06:37 +0200

    database adder v2

--- src/main/java/pl/pjwstk/woloappapi/WoloAppApiApplication.java
+++ src/main/java/pl/pjwstk/woloappapi/WoloAppApiApplication.java
@@ -2,12 +2,15 @@ package pl.pjwstk.woloappapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import pl.pjwstk.woloappapi.controller.DatabaseInitializer;
 
 @SpringBootApplication
 public class WoloAppApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WoloAppApiApplication.class, args);
-	}
 
+
+		ConfigurableApplicationContext context = SpringApplication.run(WoloAppApiApplication.class, args);
+	}
 }
--- src/main/java/pl/pjwstk/woloappapi/controller/DatabaseInitializer.java
+++ src/main/java/pl/pjwstk/woloappapi/controller/DatabaseInitializer.java
@@ -1,48 +1,40 @@
 package pl.pjwstk.woloappapi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 @Component
 public class DatabaseInitializer implements ApplicationRunner {
 
-    private final DataSource dataSource;
-
-    public DatabaseInitializer(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @Override
-    public void run(ApplicationArguments args) throws SQLException {
-        try (Connection connection = dataSource.getConnection()) {
-            boolean databaseExists = checkIfDatabaseExists(connection);
-
-            if (!databaseExists) {
-                createDatabase(connection);
-            }
+    public void run(ApplicationArguments args) throws Exception {
+        if (!checkIfDatabaseExists()) {
+            createDatabase();
+            createUser();
         }
+
+
     }
 
-    private boolean checkIfDatabaseExists(Connection connection) throws SQLException {
-        try (Statement statement = connection.createStatement()) {
-            ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM pg_database WHERE datname = 'nazwa_bazy_danych'");
-            if (resultSet.next()) {
-                int count = resultSet.getInt(1);
-                return count > 0;
-            }
-        }
-        return false;
+    private boolean checkIfDatabaseExists() {
+        String query = "SELECT 1 FROM pg_database WHERE datname = 'postgres'";
+        Integer result = jdbcTemplate.queryForObject(query, Integer.class);
+        return result != null && result == 1;
     }
 
-    private void createDatabase(Connection connection) throws SQLException {
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate("CREATE DATABASE postgres");
-        }
+    private void createDatabase() {
+        String createDatabaseQuery = "CREATE DATABASE postgres";
+        jdbcTemplate.execute(createDatabaseQuery);
+    }
+
+    private void createUser() {
+        String createUserQuery = "CREATE USER postgres WITH PASSWORD Qwe123 SUPERUSER";
+        jdbcTemplate.execute(createUserQuery);
     }
 }
