18486ae58d3c15c141c997e1d819cd02f6e5af94
Fylyp19 <77103796+Fylyp19@users.noreply.github.com>
2023-10-17 14:30:50 +0200

    little correction for test

--- src/test/java/pl/pjwstk/woloappapi/WoloAppApiApplicationTests.java
+++ src/test/java/pl/pjwstk/woloappapi/WoloAppApiApplicationTests.java
@@ -2,15 +2,16 @@ package pl.pjwstk.woloappapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static io.restassured.RestAssured.given;
 
 @SpringBootTest
 class WoloAppApiApplicationTests {
 
 	@Test
 	public void testHealthEndpoint() {
-				given()
-				.when().get("/health")
-				.then()
-				.statusCode(200);
+		given()
+		.when().get("/health")
+		.then()
+		.statusCode(200);
 	}
 }
