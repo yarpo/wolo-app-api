e43bc8291d34abd078484e7a01f1db73a3ea4c84
Fylyp19 <77103796+Fylyp19@users.noreply.github.com>
2023-11-05 21:23:24 +0100

    local-to-main

--- src/main/java/pl/pjwstk/woloappapi/utils/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package pl.pjwstk.woloappapi.utils;
-
-public class UserNotFoundException extends RuntimeException{
-
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
