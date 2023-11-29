c3289d2f2383918dab694c78d2e4738b2b9a5175
MKurshakova <mariia.kurshakova@gmail.com>
2023-11-29 12:58:06 +0100

    WOLO-115: OrganisationResponseDto, OrganisationMapper methods toOrganisation, toAddress, changes in Organisation Controller and service

--- src/main/java/pl/pjwstk/woloappapi/service/OrganisationService.java
+++ src/main/java/pl/pjwstk/woloappapi/service/OrganisationService.java
@@ -2,17 +2,26 @@ package pl.pjwstk.woloappapi.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import pl.pjwstk.woloappapi.model.Event;
-import pl.pjwstk.woloappapi.model.Organisation;
+import pl.pjwstk.woloappapi.model.*;
+import pl.pjwstk.woloappapi.repository.AdressRepository;
+import pl.pjwstk.woloappapi.repository.DistrictRepository;
 import pl.pjwstk.woloappapi.repository.OrganisationRepository;
+import pl.pjwstk.woloappapi.repository.UserRepository;
 import pl.pjwstk.woloappapi.utils.NotFoundException;
+import pl.pjwstk.woloappapi.utils.OrganisationMapper;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class OrganisationService {
     private final OrganisationRepository organisationRepository;
+    private final OrganisationMapper organisationMapper;
+    private final DistrictRepository districtRepository;
+    private final AdressRepository adressRepository;
+
+    private final UserRepository userRepository;
     public List<Organisation> getAllOrganisations() {
         return organisationRepository.findAll();
     }
@@ -22,8 +31,18 @@ public class OrganisationService {
                 .orElseThrow(() -> new NotFoundException("Organisation id not found!"));
     }
 
-    public void createOrganisation(Organisation organisation) {
+    public void createOrganisation(OrganisationRequestDto organisationDto) {
+        District district = districtRepository.findById(organisationDto.getDistrictId())
+                .orElseThrow(()-> new NotFoundException("District id not found!"));
+        Address address = organisationMapper.toAddress(organisationDto);
+        Organisation organisation = organisationMapper.toOrganisation(organisationDto);
+        address.setDistrict(district);
+        organisation.setAddress(address);
+        Optional<User> user = userRepository.findById(organisationDto.getModeratorId());
+        user.ifPresent(organisation::setModerator);
         organisationRepository.save(organisation);
+        address.getOrganisations().add(organisation);
+        adressRepository.save(address);
     }
 
     public void updateOrganisation(Organisation organisation, Long id) {
