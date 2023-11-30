9d8a15ed5dec110bade8cdde54eb568cabe5ee1a
MKurshakova <mariia.kurshakova@gmail.com>
2023-11-30 12:19:12 +0100

    WOLO-115: Organisation edit() with updateFieldIfDifferent()

--- src/main/java/pl/pjwstk/woloappapi/service/OrganisationService.java
+++ src/main/java/pl/pjwstk/woloappapi/service/OrganisationService.java
@@ -11,6 +11,8 @@ import pl.pjwstk.woloappapi.utils.OrganisationMapper;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 @Service
 @AllArgsConstructor
@@ -43,47 +45,28 @@ public class OrganisationService {
         Organisation organisation = organisationRepository.findById(id)
                 .orElseThrow(() ->
                         new IllegalArgumentException("Organisation with ID " + id + " does not exist"));
-        if(!Objects.equals(organisation.getName(), organisationDto.getName())){
-            organisation.setName(organisationDto.getName());
-        }
+        updateFieldIfDifferent(organisation::getName, organisation::setName, organisationDto.getName());
+        updateFieldIfDifferent(organisation::getDescription, organisation::setDescription,
+                organisationDto.getDescription());
+        updateFieldIfDifferent(organisation::getEmail, organisation::setEmail,
+                organisationDto.getEmail());
+        updateFieldIfDifferent(organisation.getAddress()::getStreet,
+                organisation.getAddress()::setStreet, organisationDto.getStreet());
+        updateFieldIfDifferent(organisation.getAddress()::getHomeNum, organisation.getAddress()::setHomeNum,
+                organisationDto.getHomeNum());
+        updateFieldIfDifferent(organisation.getAddress()::getAddressDescription,
+                organisation.getAddress()::setAddressDescription, organisationDto.getAddressDescription());
 
-        if(!Objects.equals(organisation.getDescription(), organisationDto.getDescription())){
-            organisation.setDescription(organisationDto.getDescription());
-        }
-
-        if(!organisation.getEmail().equals(organisationDto.getEmail())){
-            organisation.setEmail(organisationDto.getEmail());
-        }
-
-        if(!Objects.equals(organisation.getPhoneNumber(), organisationDto.getPhoneNumber())){
-            organisation.setPhoneNumber(organisationDto.getPhoneNumber());
-        }
-
-        if(!Objects.equals(organisation.getAddress().getStreet(), organisationDto.getStreet())){
-            organisation.getAddress().setStreet(organisationDto.getStreet());
-        }
-
-        if(!Objects.equals(organisation.getAddress().getHomeNum(), organisationDto.getHomeNum())){
-            organisation.getAddress().setHomeNum(organisationDto.getHomeNum());
-        }
-
-        if(!Objects.equals(organisation.getAddress().getAddressDescription(),
-                organisationDto.getAddressDescription())){
-            organisation.getAddress().setAddressDescription(organisationDto.getAddressDescription());
-        }
-
-        if(!Objects.equals(organisation.getAddress().getDistrict().getId(), organisationDto.getDistrictId())){
-            District district = districtService.getDistrictById(organisationDto.getDistrictId());
+        updateFieldIfDifferent(() -> organisation.getAddress().getDistrict().getId(), dId -> {
+            District district = districtService.getDistrictById(dId);
             organisation.getAddress().setDistrict(district);
-        }
+        }, organisationDto.getDistrictId());
 
-        if(!Objects.equals(organisation.getModerator().getId(), organisationDto.getModeratorId())){
-            userRepository.findById(organisationDto.getModeratorId()).ifPresent(organisation::setModerator);
-        }
-
-        if(!Objects.equals(organisation.getLogoUrl(), organisationDto.getLogoUrl())){
-            organisation.setLogoUrl(organisationDto.getLogoUrl());
-        }
+        updateFieldIfDifferent(() -> organisation.getModerator().getId(),
+                mId -> userRepository.findById(mId).ifPresent(organisation::setModerator),
+                organisationDto.getModeratorId());
+        updateFieldIfDifferent(organisation::getLogoUrl, organisation::setLogoUrl,
+                organisationDto.getLogoUrl());
 
         organisationRepository.save(organisation);
     }
@@ -101,5 +84,12 @@ public class OrganisationService {
                 .orElseThrow(() -> new NotFoundException("Organizer id not found!"));
     }
 
+    private <T> void updateFieldIfDifferent(Supplier<T> currentSupplier,
+                                            Consumer<T> updateConsumer,
+                                            T newValue) {
+        if (!Objects.equals(currentSupplier.get(), newValue)) {
+            updateConsumer.accept(newValue);
+        }
+    }
 
 }
