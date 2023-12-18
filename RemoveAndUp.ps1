a57762740b5c98b4d94c63bc4fd2b719f1407979
Jakwisnie <kubaw199808@gmail.com>
2023-12-18 04:03:11 +0100

    Requested changes added

--- RemoveAndUp.ps1
+++ RemoveAndUp.ps1
@@ -1,21 +1,11 @@
-# Nazwa skryptu: RemoveAndUp.ps1
-
-# Ścieżka do katalogu, który chcesz usunąć rekurencyjnie
 $directoryPath = "./data"
-
-# Sprawdź, czy katalog istnieje przed próbą usunięcia
 if (Test-Path $directoryPath) {
-    # Usuń katalog rekurencyjnie
     Remove-Item -Path $directoryPath -Recurse -Force
-    Write-Host "Katalog $directoryPath został pomyślnie usunięty."
-
-    # Usuń tylko obraz "wolo-app-api-backend"
+    Write-Host "Catalog $directoryPath removed."
     $imageToRemove = "wolo-app-api-backend"
     docker rmi -f $(docker images -q $imageToRemove)
-    Write-Host "Obraz $imageToRemove został pomyślnie usunięty."
-
-
+    Write-Host "Image $imageToRemove removed."
 } else {
-    Write-Host "Katalog $directoryPath nie istnieje."
+    Write-Host "Catalog $directoryPath not exist."
 }
     docker-compose --env-file .env --profile standalone up
