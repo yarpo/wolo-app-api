818fe053a5bc54a286f181f50534a43bba12ac3a
Jakwisnie <kubaw199808@gmail.com>
2024-01-04 17:34:51 +0100

    Small fix

--- src/main/java/pl/pjwstk/woloappapi/repository/ShiftToUserRepository.java
+++ src/main/java/pl/pjwstk/woloappapi/repository/ShiftToUserRepository.java
@@ -4,9 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import pl.pjwstk.woloappapi.model.ShiftToUser;
-import pl.pjwstk.woloappapi.model.User;
+import pl.pjwstk.woloappapi.model.UserEntity;
 
 @Repository
 public interface ShiftToUserRepository extends JpaRepository<ShiftToUser, Long> {
-    void deleteByUser(User user);
+    void deleteByUser(UserEntity user);
 }
