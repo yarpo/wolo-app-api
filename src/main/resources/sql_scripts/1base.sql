8d59ac47406a97ac5543881e79a2cc9f1c0d6abb
MKurshakova <mariia.kurshakova@gmail.com>
2024-02-05 16:54:04 +0100

    WOLO-00 sql scripts

--- src/main/resources/sql_scripts/1base.sql
+++ src/main/resources/sql_scripts/1base.sql
@@ -164,7 +164,17 @@ INSERT INTO role ("name") VALUES
 INSERT INTO district ( "name", city) VALUES
                                          ( 'Centrum', 'Warszawa'),
                                          ( 'Wrzeszcz', 'Gdańsk'),
-                                         ( 'Śródmieście', 'Gdańsk');
+                                         ( 'Śródmieście', 'Gdańsk'),
+                                         ( 'Oliwa', 'Gdańsk'),
+                                         ( 'Aniołki', 'Gdańsk'),
+                                         ( 'Morena', 'Gdańsk'),
+                                         ( 'Orunia', 'Gdańsk'),
+                                         ( 'Jelitkowo', 'Gdańsk'),
+                                         ( 'Przymorze', 'Gdańsk'),
+                                         ( 'Zaspa', 'Gdańsk'),
+                                         ( 'Ujeścisko-Łostowice', 'Gdańsk'),
+                                         ( 'Siedlce', 'Gdańsk'),
+                                         ('Nowy Port', 'Gdańsk');
 
 INSERT INTO category ( "name") VALUES
                                    ( 'Sport'),
