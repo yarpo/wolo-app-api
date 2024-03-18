e5e5c40a54efe7944d006b7e97f74b504b5c2d17
Kinga <Kinga.Wajda@protonmail.com>
2024-03-18 17:50:43 +0100

    Add base admin and example shifts

--- src/main/resources/sql_inserts/004-insert-base_admin.sql
+++ src/main/resources/sql_inserts/004-insert-base_admin.sql
@@ -2,7 +2,7 @@
 
 --changeset kwajda:004
 INSERT INTO "user" ( firstname, lastname, email, phone_number, is_pesel_verified, is_agreement_signed, is_adult, organisation_id, "password") VALUES
-                                                                                                                                                  ( 'Admin', 'Admin', 'admin@example.com', '000000000', true, true, true, null, 'null');
+                                                                                                                                                  ( 'Admin', 'Admin', 'admin@example.com', '000000000', true, true, true, null, '$2a$10$mX6Rc53zw6lVn6YBk3hZ6.NTf9P7DpH/ahRRovBwzUqk74YZyhHKi');
 
 INSERT INTO user_to_role (role_id, user_id) VALUES
                                                 (1, 1);
