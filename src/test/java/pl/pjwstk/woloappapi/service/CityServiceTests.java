e141f3274127f5ef660a2f43ef862e2dc5445614
Fylyp19 <77103796+Fylyp19@users.noreply.github.com>
2024-04-27 17:01:11 +0200

    Update CityServiceTests.java

--- src/test/java/pl/pjwstk/woloappapi/service/CityServiceTests.java
+++ src/test/java/pl/pjwstk/woloappapi/service/CityServiceTests.java
@@ -64,16 +64,16 @@ public class CityServiceTests {
 
     @Test
     public void testCreateCity() {
-        var cityDto = new CityDto(null, "New City", List.of(1L, 2L));
+        var cityDto = new CityDto(null, "New City", List.of("District1", "District2"));
 
         when(dictionariesMapper.toCity(cityDto)).thenReturn(
                 City.builder()
                         .name(cityDto.getName())
                         .isOld(false)
         );
-        when(districtService.getDistrictById(1L))
+        when(districtService.getDistrictByName("District1"))
                 .thenReturn(new District(1L, "District1", false, null, null));
-        when(districtService.getDistrictById(2L))
+        when(districtService.getDistrictByName("District2"))
                 .thenReturn(new District(2L, "District2", false, null, null));
 
         cityService.createCity(cityDto);
@@ -84,7 +84,7 @@ public class CityServiceTests {
     @Test
     public void testUpdateCity() {
         var cityId = 1L;
-        var cityDto = new CityDto(cityId, "Updated City", List.of(1L, 2L));
+        var cityDto = new CityDto(cityId, "Updated City", List.of("District1", "District2"));
 
         City existingCity = new City(cityId, "City1", false, new ArrayList<>(), null);
         existingCity.getDistricts().add(new District(1L, "District1", false, existingCity, null));
