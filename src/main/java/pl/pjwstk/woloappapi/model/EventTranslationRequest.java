5249db5b6024566c6cfd0f2609fd5715b13da0b6
MKurshakova <mariia.kurshakova@gmail.com>
2024-05-02 11:50:53 +0200

    WOLO-00 bug fix in EventController /add

--- src/main/java/pl/pjwstk/woloappapi/model/EventTranslationRequest.java
+++ src/main/java/pl/pjwstk/woloappapi/model/EventTranslationRequest.java
@@ -1,14 +1,7 @@
 package pl.pjwstk.woloappapi.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
-
 import java.util.List;
 
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class EventTranslationRequest {
 
     private String name;
@@ -20,4 +13,47 @@ public class EventTranslationRequest {
     private String imageUrl;
 
     private String language;
+
+    public EventTranslationRequest() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getShiftDirections() {
+        return shiftDirections;
+    }
+
+    public void setShiftDirections(List<String> shiftDirections) {
+        this.shiftDirections = shiftDirections;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
 }
