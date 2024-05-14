da0a8e5bf000178253aff45d5e8b014b86c37a12
Fylyp19 <77103796+Fylyp19@users.noreply.github.com>
2024-05-14 22:52:11 +0200

    optimalization for EventUtil

--- src/main/java/pl/pjwstk/woloappapi/utils/EmailUtil.java
+++ src/main/java/pl/pjwstk/woloappapi/utils/EmailUtil.java
@@ -16,91 +16,54 @@ public class EmailUtil {
 
 
     public void sendResetPasswordEmail(String email) throws MessagingException {
-        var mimeMessage = javaMailSender.createMimeMessage();
-        var mimeMessageHelper = new MimeMessageHelper(mimeMessage);
-        mimeMessageHelper.setTo(email);
-        mimeMessageHelper.setSubject("Set Password");
-        mimeMessageHelper.setText("""
+        mailSender(email, null, "Set Password", """
         <div>
           <a href="http://localhost:8080/reset-password?email=%s" target="_blank">click link to set password</a>
         </div>
-        """.formatted(email), true);
-
-        javaMailSender.send(mimeMessage);
+        """);
     }
 
     public void sendDeleteEventMessage(String email, Long id) throws MessagingException{
-        var mimeMessage = javaMailSender.createMimeMessage();
-        var mimeMessageHelper = new MimeMessageHelper(mimeMessage);
-        mimeMessageHelper.setTo(email);
-        mimeMessageHelper.setSubject("Еhe event you signed up for has been cancelled");
-        mimeMessageHelper.setText("%s has been cancelled".formatted(eventRepository
-                .findById(id).map(Event::getNameEN)
-                .orElse("Еhe event you signed up for ")));
-        javaMailSender.send(mimeMessage);
+        mailSender(email, id, "The event you signed up for has been cancelled",
+                "%s has been cancelled");
     }
 
     public void sendEditEventMail(String email, Long id) throws MessagingException{
-        var mimeMessage = javaMailSender.createMimeMessage();
-        var mimeMessageHelper = new MimeMessageHelper(mimeMessage);
-        mimeMessageHelper.setTo(email);
-        mimeMessageHelper.setSubject("Еhe event you signed up for has been changed");
-        mimeMessageHelper.setText("%s has been changed".formatted(eventRepository
-                .findById(id).map(Event::getNameEN)
-                .orElse("Еhe event you signed up for ")));
-        javaMailSender.send(mimeMessage);
+        mailSender(email, id, "The event you signed up for has been changed", "%s has been changed");
     }
 
     public void sendPeselVerificationEmail(String email, Long id) throws MessagingException {
-        var mimeMessage = javaMailSender.createMimeMessage();
-        var mimeMessageHelper = new MimeMessageHelper(mimeMessage);
-        mimeMessageHelper.setTo(email);
-        mimeMessageHelper.setSubject("Еhe event requirements you signed up for has been changed");
-        mimeMessageHelper.setText("If you want to participate in %s, you must verify your PESEL and re-register for this event"
-                .formatted(eventRepository
-                    .findById(id).map(Event::getNameEN)
-                    .orElse("event ")));
-        javaMailSender.send(mimeMessage);
+        mailSender(email, id, "The event requirements you signed up for has been changed",
+                "If you want to participate in %s, you must verify your PESEL and re-register for this event");
     }
 
     public void sendAgreementNeededEmail(String email, Long id) throws MessagingException{
-        var mimeMessage = javaMailSender.createMimeMessage();
-        var mimeMessageHelper = new MimeMessageHelper(mimeMessage);
-        mimeMessageHelper.setTo(email);
-        mimeMessageHelper.setSubject("Еhe event requirements you signed up for has been changed");
-        mimeMessageHelper.setText("If you want to participate in %s, you must sign a volunteer agreement and re-register for this event"
-                .formatted(eventRepository
-                        .findById(id).map(Event::getNameEN)
-                        .orElse("event ")));
-        javaMailSender.send(mimeMessage);
-
+        mailSender(email, id, "The event requirements you signed up for has been changed",
+                "If you want to participate in %s, you must sign a volunteer agreement and re-register for this event");
     }
 
     public void sendUpdateCapacityEmail(String email, Long id) throws MessagingException{
-        var mimeMessage = javaMailSender.createMimeMessage();
-        var mimeMessageHelper = new MimeMessageHelper(mimeMessage);
-        mimeMessageHelper.setTo(email);
-        mimeMessageHelper.setSubject("Important event update");
-        mimeMessageHelper.setText(("Unfortunately, the number of participants in %s has been reduced. " +
+        mailSender(email, id, "Important event update",
+                "Unfortunately, the number of participants in %s has been reduced. " +
                 "You can no longer take part in it. Sorry for the inconvenience. " +
-                "We invite you to our application where you can find others interesting events for you")
-                .formatted(eventRepository
-                        .findById(id).map(Event::getNameEN)
-                        .orElse("event ")));
-        javaMailSender.send(mimeMessage);
+                "We invite you to our application where you can find others interesting events for you");
     }
 
     public void sendMinAgeMail(String email, Long id) throws MessagingException{
+        mailSender(email, id, "Important event update",
+                "Unfortunately, the minimum age required for %s has been changed. " +
+                "You can no longer take part in it. Sorry for the inconvenience. " +
+                "We invite you to our application where you can find others interesting events for you");
+    }
+
+    public void mailSender(String email, Long id, String subject, String message) throws MessagingException{
         var mimeMessage = javaMailSender.createMimeMessage();
         var mimeMessageHelper = new MimeMessageHelper(mimeMessage);
         mimeMessageHelper.setTo(email);
-        mimeMessageHelper.setSubject("Important event update");
-        mimeMessageHelper.setText(("Unfortunately, the minimum age required for %s has been changed. " +
-                "You can no longer take part in it. Sorry for the inconvenience. " +
-                "We invite you to our application where you can find others interesting events for you")
-                .formatted(eventRepository
-                        .findById(id).map(Event::getNameEN)
-                        .orElse("event ")));
+        mimeMessageHelper.setSubject(subject);
+        String eventName = (id != null) ?
+                eventRepository.findById(id).map(Event::getNameEN).orElse("event") : email;
+        mimeMessageHelper.setText(message.formatted(eventName));
         javaMailSender.send(mimeMessage);
     }
 }
